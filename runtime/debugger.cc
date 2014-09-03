dee99100cc4d25c298d750fe44b908f302ca4e5c
abdelhaf <none@none>
2014-09-03 13:32:18 -0400

    changing status of the daemon

--- runtime/debugger.cc
+++ runtime/debugger.cc
@@ -1686,6 +1686,7 @@ JDWP::JdwpThreadStatus Dbg::ToJdwpThreadStatus(ThreadState state) {
     case kWaitingInMainSignalCatcherLoop:
     case kWaitingPerformingGc:
     case kWaitingInMainGCMMPCatcherLoop:
+    case kWaitingForGCMMPCatcherOutput:
     case kWaiting:
       return JDWP::TS_WAIT;
       // Don't add a 'default' here so the compiler can spot incompatible enum changes.
--- runtime/native/java_lang_Thread.cc
+++ runtime/native/java_lang_Thread.cc
@@ -83,6 +83,7 @@ static jint Thread_nativeGetStatus(JNIEnv* env, jobject java_thread, jboolean ha
     case kWaitingForSignalCatcherOutput:  return kJavaWaiting;
     case kWaitingInMainSignalCatcherLoop: return kJavaWaiting;
     case kWaitingInMainGCMMPCatcherLoop:	return kJavaWaiting;
+    case kWaitingForGCMMPCatcherOutput:   return kJavaWaiting;
     case kSuspended:                      return kJavaRunnable;
     // Don't add a 'default' here so the compiler can spot incompatible enum changes.
   }
