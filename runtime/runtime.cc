c5a8b37b61ecb08a884ed5d04da47d76cb3ed06d
abdelhaf <none@none>
2014-09-04 13:31:14 -0400

    Disabling concurrent GC

--- runtime/runtime.cc
+++ runtime/runtime.cc
@@ -572,7 +572,7 @@ Runtime::ParsedOptions* Runtime::ParsedOptions::Create(const Options& options, b
     } else if (Runtime::ParsedOptions::ParseMProfileOption(option, &parsed->mprofiler_options_)) {
     	if(parsed->mprofiler_options_->mprofile_grow_method_ & art::mprofiler::GC_GROW_METHOD_FG_GC) {
     		 parsed->is_concurrent_gc_enabled_ = false;
-    		 LOG(INFO) << "XXX Diablie concurrent GC ";
+    		 LOG(INFO) << "XXX Diablie concurrent GC";
     	}
     	LOG(INFO) << "XXXX Done Parsing -Xgcmmp option: " << option;
     } else if (option == "-XX:+DisableExplicitGC") {
@@ -921,7 +921,7 @@ bool Runtime::Init(const Options& raw_options, bool ignore_unrecognized) {
                        options->heap_target_utilization_,
                        options->heap_maximum_size_,
                        options->image_,
-                       options->is_concurrent_gc_enabled_ && options->,
+                       options->is_concurrent_gc_enabled_,
                        options->parallel_gc_threads_,
                        options->conc_gc_threads_,
                        options->low_memory_mode_,
