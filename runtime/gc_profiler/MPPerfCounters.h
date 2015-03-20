0d635363fae3f8fcd08263255470298f6209e0eb
abdelhaf <none@none>
2015-03-20 12:40:26 -0400

    checking GC costs

--- runtime/gc_profiler/MPPerfCounters.h
+++ runtime/gc_profiler/MPPerfCounters.h
@@ -23,6 +23,102 @@
 namespace art {
 namespace mprofiler {
 
+typedef struct GCMMP_Options_s {
+	int mprofile_type_;
+	int mprofile_grow_method_;
+	int mprofile_gc_affinity_;
+} GCMMP_Options;
+
+/**
+ * enum used to define the concurrency of the virtual machine
+ */
+typedef enum {
+	GC_GROW_METHOD_DEFAULT 	= 0,
+	GC_GROW_METHOD_BG_GC 		= 1,
+	GC_GROW_METHOD_WITH_PID = 2,
+	GC_GROW_METHOD_WITH_HEUR= 4,
+	GC_GROW_METHOD_FG_GC		= 8,
+	GC_GROW_METHOD_EXPL_OFF	= 16,
+	GC_GROW_METHOD_SUPER_BG_GC 		= 32
+} GC_GROW_METHODS;
+
+
+
+/*
+ * enum of the events we are profiling per mutator. we can look for activities.
+ * Make sure that GCMMP_GC_BRK_MAXIMUM always at the bottom of the definition
+ */
+typedef enum {
+	GCMMP_GC_BRK_NONE = 0,
+	GCMMP_GC_BRK_SUSPENSION = 1,
+	GCMMP_GC_BRK_WAIT_CONC,
+	GCMMP_GC_BRK_GC_EXPL,
+	GCMMP_GC_BRK_GC_HAT,
+	GCMMP_GC_BRK_HEAP_LOCK,
+	GCMMP_GC_BRK_SAFEPOINT,
+	GCMMP_GC_BRK_MAXIMUM
+} GCMMP_BREAK_DOWN_ENUM;
+
+
+/*
+ * represents the status of the thread. We need to know whether a thread
+ * is profiled, stopped, of still running from the profiler point of view.
+ */
+typedef enum GCMMPThreadProfStateT {
+	GCMMP_TH_ALLOC = 0,
+	GCMMP_TH_STARTING,
+	GCMMP_TH_RUNNING,
+	GCMMP_TH_STOPPED,
+	GCMMP_TH_REMOVED
+} GCMMPThreadProfState;
+
+/*
+ * Flags used to check the functionality of the mprofiler->flags_
+ */
+typedef enum GCMMPFlagsEnum_s {
+	GCMMP_FLAGS_NONE = 0,
+	GCMMP_FLAGS_CREATE_DAEMON = 1, //should we create a daemon profiler?
+	GCMMP_FLAGS_HAS_DAEMON = 2,		 //does it possess a daemon thread
+	GCMMP_FLAGS_ATTACH_PROF_DAEMON = 4, // should we attach the profile daemon
+	GCMMP_FLAGS_MARK_ALLOC_WINDOWS = 8, //should we mark the allocation chunks
+	GCMMP_FLAGS_ATTACH_GCDAEMON = 16
+} GCMMPFlagsEnum;
+
+
+
+/*
+ * Flags used to check the functionality of the mprofiler->flags_
+ */
+typedef enum GCMMPThProfileTag_s {
+	GCMMP_THREAD_DEFAULT = 0,
+	GCMMP_THREAD_MAIN = 1,
+	GCMMP_THREAD_GCDAEMON = 2, //should we create a daemon profiler?
+	GCMMP_THREAD_GCTRIM = 3		 //does it possess a daemon thread
+} GCMMPThProfileTag;
+
+/*
+ * Struct used to hold the temporary values when we enter the block of an
+ * activity. When we exit we add the delta to the accumulated field.
+ */
+typedef struct PACKED(4) GCMMP_ProfileActivity_S {
+	/* Used as a temp to hold the counter vaue when we enter the activity block */
+	uint64_t startMarker;
+	/* represents the total value of the activity */
+	uint64_t finalMarker;
+} GCMMP_ProfileActivity;
+
+/*
+ * Struct used to hold the temporary values when we enter the block of an
+ * activity. When we exit we add the delta to the accumulated field.
+ */
+typedef struct PACKED(4) GCPauseThreadMarker_S {
+	/* Used as a temp to hold the counter vaue when we enter the activity block */
+	uint64_t startMarker;
+	/* represents the total value of the activity */
+	uint64_t finalMarker;
+	/* the type of the pause */
+	GCMMP_BREAK_DOWN_ENUM type;
+} GCPauseThreadMarker;
 
 class PerfEventLogger {
  public:
