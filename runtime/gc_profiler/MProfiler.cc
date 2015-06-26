56a240100cea0ce0c1d382387280311a2dec659f
abdelhaf <none@none>
2015-06-26 19:41:03 -0400

    GCPRofiler: pushing the handling to the thread level

--- runtime/gc_profiler/MProfiler.cc
+++ runtime/gc_profiler/MProfiler.cc
@@ -2680,7 +2680,7 @@ void GCHistogramThreadManager::initHistograms(void){
 	memset((void*)lastWindowHistTable, 0, lastWindowHistSize);
 
 
-	for(size_t i = 0; i < kGCMMPMaxHistogramEntries; i++){
+	for(int i = 0; i < kGCMMPMaxHistogramEntries; i++){
 		histogramTable[i].index 			= (i+1) * 1.0;
 		lastWindowHistTable[i].index  = (i+1) * 1.0;
 	}
