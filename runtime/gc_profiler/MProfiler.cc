1b44d4fa61a35eb718a3f862e84e914144f777f5
abdelhaf <none@none>
2015-07-05 09:13:10 -0400

    GCProfiler, add the ClassProfiler. refactor Histogrammanager

--- runtime/gc_profiler/MProfiler.cc
+++ runtime/gc_profiler/MProfiler.cc
@@ -2783,7 +2783,7 @@ inline void GCClassTableManager::addObjectClassPair(mirror::Class* klass,
   	_histRec = (GCPHistogramRec*) calloc(1, sizeof(GCPHistogramRec));
   	LOG(ERROR) << "start Hash=" << klassHash;
   	//if(false)
-  		classTable_.insert(std::make_pair(klassHash, GCPHistogramClass()));
+  		classTable_.insert(pair<size_t, GCPHistogramClass>(klassHash, GCPHistogramClass()));
   	classTableTest_.insert(std::make_pair(klassHash, klassHash));
   	LOG(ERROR) << "Done Hash=" << klassHash;
   }
