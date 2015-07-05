eb2cd25af43cc0d2eb5a7949522e64947e7ce0b8
abdelhaf <none@none>
2015-07-05 10:03:14 -0400

    GCProfiler, add the ClassProfiler. refactor Histogrammanager

--- runtime/gc_profiler/MProfiler.cc
+++ runtime/gc_profiler/MProfiler.cc
@@ -2769,7 +2769,8 @@ inline void GCClassTableManager::addObjectClassPair(mirror::Class* klass,
 
 	//if(directory_.empty()) {
 		LOG(ERROR) << "Adding the phone book:" << directory_.size();
-		directory_.insert(std::pair<size_t, mirror::Class*>(directory_.size(),    klass));
+		size_t sizeT = directory_.size();
+		directory_.insert(std::std::make_pair(sizeT,    klass));
 		//directory_.insert(std::pair<size_t, PhoneNum>(2,    PhoneNum("555-9999")));
 		//directory_.insert(std::pair<size_t, PhoneNum>(3,  PhoneNum("555-9678")));
 		//LOG(ERROR) << "Done Adding the phone book";
