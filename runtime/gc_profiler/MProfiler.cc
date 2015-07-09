87072dadec9a3049cc1085118c75ef15e08c8948
abdelhaf <none@none>
2015-07-09 14:47:02 -0400

    GCProfiler, Adding parameter for cohort sizes

--- runtime/gc_profiler/MProfiler.cc
+++ runtime/gc_profiler/MProfiler.cc
@@ -3194,7 +3194,7 @@ void GCClassTableManager::printClassNames(void) {
 			LOG(ERROR) << "XXXX NULL Hash: " << it.first;
 		else {
 			LOG(ERROR) << gcpLogDataRecord(LOG(ERROR)<< "Name: " <<
-					Class::PrettyClass(_klass),
+					mirror::Class::PrettyClass(_klass),
 					_rec->countData_.gcpGetDataRecP());
 		}
 //		mirror::Class* _klass = Runtime::Current()->GetClassLinker()->
