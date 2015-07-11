17b9b85353c22fc0d7975169c79dd953ad368c12
abdelhaf <none@none>
2015-07-11 12:01:08 -0400

    testing GCPRofiler

--- runtime/gc_profiler/MProfilerTypes.h
+++ runtime/gc_profiler/MProfilerTypes.h
@@ -583,13 +583,13 @@ public:
 
 	void gcpLogDataRecord(std::ostream& os) {
 		GCPHistogramRec* _dataRec = &histData_->dataRec_;
-		os << "index: "<< StringPrintf("%lu",_dataRec->index) << ";cntLive: " << _dataRec->cntLive <<
+		os << "index: "<< StringPrintf("%llu",_dataRec->index) << ";cntLive: " << _dataRec->cntLive <<
 				"; cntTotal: "<< _dataRec->cntTotal<< "; pcntLive: " <<
 				_dataRec->pcntLive << "; pcntTotal: " << _dataRec->pcntTotal;
 	}
 
 	void gcpLogDataRecord(std::ostream& os, GCPHistogramRec* _dataRec) {
-		os << "index: "<< StringPrintf("%lu",_dataRec->index) << "; cntLive: " << _dataRec->cntLive <<
+		os << "index: "<< StringPrintf("%llu",_dataRec->index) << "; cntLive: " << _dataRec->cntLive <<
 				"; cntTotal: "<< _dataRec->cntTotal<< "; pcntLive: " <<
 				_dataRec->pcntLive << "; pcntTotal: " << _dataRec->pcntTotal;
 	}
