827191cf46816b4a89941f32afb0a55835de1def
abdelhaf <none@none>
2015-07-16 14:22:07 -0400

    RefDistanceProfiler: start implementation

--- runtime/gc_profiler/MProfiler.h
+++ runtime/gc_profiler/MProfiler.h
@@ -298,7 +298,7 @@ public:
   size_t getRelevantAllocBytes(void);
   void setThreadAffinity(art::Thread* th, bool complementary);
 
-  static bool MProfRefDistance(const mirror::Object*, const mirror::Object*);
+  static bool MProfRefDistance(const art::mirror::Object*, const art::mirror::Object*);
 	static bool IsMProfRunning();
 	static bool IsMProfilingTimeEvent();
 	static bool IsMProfHWRunning();
