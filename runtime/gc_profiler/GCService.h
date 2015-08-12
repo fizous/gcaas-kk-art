d5a5df10112b874ca24b3e9b8285ab4139584412
abdelhaf <none@none>
2015-08-11 22:56:41 -0400

    adding GCService source file

--- runtime/gc_profiler/GCService.h
+++ runtime/gc_profiler/GCService.h
@@ -8,6 +8,21 @@
 #ifndef ART_RUNTIME_GC_PROFILER_GCSERVICE_H_
 #define ART_RUNTIME_GC_PROFILER_GCSERVICE_H_
 
+#include <string>
+#include <list>
+#include "locks.h"
+#include "base/mutex.h"
+#include "base/unix_file/fd_file.h"
+#include "os.h"
+#include "base/logging.h"
+#include "thread_state.h"
+#include "cutils/SharedProcessMutex.h"
+#include "gc_profiler/MProfilerTypes.h"
+#include "gc_profiler/MProfilerHeap.h"
+#include "cutils/system_clock.h"
+#include "utils.h"
+#include "offsets.h"
+
 namespace art {
 namespace mprofiler {
 
