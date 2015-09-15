56263e2b76e82945217771902e3395fc3c922a9f
abdelhaf <none@none>
2015-09-15 19:47:06 -0400

    Adding Base Class for base heapBitmap

--- runtime/gc/accounting/heap_bitmap.cc
+++ runtime/gc/accounting/heap_bitmap.cc
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#include "space_bitmap-inl.h"
 #include "heap_bitmap.h"
 
 #include "gc/space/space.h"
