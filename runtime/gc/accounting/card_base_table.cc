baa5a0f8f6759ab8b44675d76f8900a9b6f460d9
abdelhaf <none@none>
2015-09-22 15:04:59 -0400

    CardBaseTable: Allow resharing the CardTable data

--- runtime/gc/accounting/card_base_table.cc
+++ runtime/gc/accounting/card_base_table.cc
@@ -23,9 +23,9 @@ namespace accounting {
 
 
 CardBaseTable::~CardBaseTable() {
-  MemMap::AshmemDestructData(&fields_->mem_map_, false);
-  free(fields_);
-  fields_ = NULL;
+//  MemMap::AshmemDestructData(&fields_->mem_map_, false);
+//  free(fields_);
+//  fields_ = NULL;
 }
 
 void CardBaseTable::ClearCardTable() {
