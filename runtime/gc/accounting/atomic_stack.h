e74d81ea5c8b35b72f6b64c0cf5f0c2dbb0138b4
abdelhaf <none@none>
2015-09-27 15:44:21 -0400

    Adding StructuredMarkStack

--- runtime/gc/accounting/atomic_stack.h
+++ runtime/gc/accounting/atomic_stack.h
@@ -110,7 +110,7 @@ class StructuredAtomicStack {
     stack_data_->back_index_ = 0;
     stack_data_->debug_is_sorted_ = true;
     if(stack_data_->is_shared_) {
-      size_t _mem_length =  sizeof(T) * stack_data_->capacity_;
+      size_t _mem_length =  sizeof(mirror::Object*) * stack_data_->capacity_;
       posix_madvise(stack_data_->begin_, _mem_length,
           POSIX_MADV_DONTNEED);
       //memset(stack_data_->begin_, 0, sizeof(T) * stack_data_->capacity_);
