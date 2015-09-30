65260cd613c444724633278c83c4d7ea61cd7791
abdelhaf <none@none>
2015-09-30 13:36:15 -0400

    GCService: sending space_index as parameter

--- runtime/gc/service/service_client.cc
+++ runtime/gc/service/service_client.cc
@@ -8,6 +8,7 @@
 #include "ipcfs/ipcfs.h"
 #include "gc/space/dlmalloc_space.h"
 #include "gc/service/service_client.h"
+#include "gc/service/global_allocator.h"
 #include "runtime.h"
 
 namespace art {
