743c092115d933d6286882304ad6d36daf5d87fa
abdelhaf <none@none>
2015-10-05 20:06:03 -0400

    adding ipc_mark_sweep

--- runtime/gc/service/service_client.cc
+++ runtime/gc/service/service_client.cc
@@ -20,11 +20,10 @@ namespace gcservice {
 GCServiceClient* GCServiceClient::service_client_ = NULL;
 
 
-
 GCServiceClient::GCServiceClient(gc::space::SharableDlMallocSpace* sharable_space,
     int index) : index_(index), sharable_space_(sharable_space) {
   collector_ =
-      new gc::collector::IPCMarkSweep(&sharable_space_->sharable_space_data_->heap_meta_);
+      new gc::collector::IPCMarkSweep(&(sharable_space_->sharable_space_data_->heap_meta_));
 //  heap_meta_ = GCService::service_->GetAllocator()->AllocateHeapMeta();
 //  GCSERV_CLIENT_ILOG << " address of the heap meta is: " <<
 //      reinterpret_cast<void*>(heap_meta_);
