3d43caf20411f0f0c8306bfdc4d80b351a65f0be
abdelhaf <none@none>
2015-10-08 12:47:18 -0400

    ServerCollector: add server collector to the code

--- runtime/gc/service/gcservice_daemon.cc
+++ runtime/gc/service/gcservice_daemon.cc
@@ -27,7 +27,7 @@ GCServiceDaemon* GCServiceDaemon::CreateServiceDaemon(GCServiceProcess* process)
 GCSrvceAgent* GCServiceDaemon::GetAgentByPid(int pid) {
   for (const auto& client : client_agents_) {
      if(client.binding_.pair_mapps_->first->process_id_ == pid) {
-       return &client;
+       return client;
      }
   }
   return NULL;
