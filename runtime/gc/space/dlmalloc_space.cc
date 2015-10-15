5e027eaa49d3bced9ab70647871e3b417c68b2f3
abdelhaf <none@none>
2015-10-15 16:04:36 -0400

    ServerCollector: change the code of swapping the bitmaps

--- runtime/gc/space/dlmalloc_space.cc
+++ runtime/gc/space/dlmalloc_space.cc
@@ -927,6 +927,39 @@ void SharableDlMallocSpace::SwapBitmaps () {
   mark_bitmap_->SetName(temp_name);
 }
 
+
+void SharableDlMallocSpace::BindLiveToMarkBitmaps(void) {
+  LOG(ERROR) << " ~~~~~~ SharableDlMallocSpace::BindLiveToMarkBitmaps ~~~~~~~";
+
+  memcpy(&sharable_space_data_->temp_bitmap_, &sharable_space_data_->mark_bitmap_,
+      SERVICE_ALLOC_ALIGN_BYTE(accounting::GCSrvceBitmap));
+  memcpy(&sharable_space_data_->mark_bitmap_, &sharable_space_data_->live_bitmap_,
+      SERVICE_ALLOC_ALIGN_BYTE(accounting::GCSrvceBitmap));
+
+  accounting::SPACE_BITMAP* _mark_bitmap_content = mark_bitmap_.release();
+  temp_bitmap_.reset(_mark_bitmap_content);
+  mark_bitmap_.reset(live_bitmap_.get());
+
+
+
+}
+
+accounting::SPACE_BITMAP* SharableDlMallocSpace::UnBindBitmaps(void) {
+  LOG(ERROR) << " ~~~~~~ SharableDlMallocSpace::UnBindBitmaps ~~~~~~~";
+  if (temp_bitmap_.get() != NULL) {
+    // At this point, the temp_bitmap holds our old mark bitmap.
+    accounting::SPACE_BITMAP* new_bitmap = mark_bitmap_.release();
+    mark_bitmap_.reset(temp_bitmap_.release());
+
+    memcpy(&sharable_space_data_->mark_bitmap_, &sharable_space_data_->temp_bitmap_,
+        SERVICE_ALLOC_ALIGN_BYTE(accounting::GCSrvceBitmap));
+
+    return new_bitmap;
+  }
+  return NULL;
+}
+
+
 /*
 void SharableDlMallocSpace::BindLiveToMarkBitmaps(void) {
   LOG(ERROR) << " ~~~~~~ SharableDlMallocSpace::BindLiveToMarkBitmaps ~~~~~~~";
