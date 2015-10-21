b72224df11a2ad239f37e18ee1dec8d5f91707cb
abdelhaf <none@none>
2015-10-21 16:24:09 -0400

    ServerCollector: change the code of swapping the bitmaps..remove changes done tgo the explicit collection

--- runtime/gc/heap.cc
+++ runtime/gc/heap.cc
@@ -155,10 +155,16 @@ Heap::Heap(size_t initial_size, size_t growth_limit, size_t min_free, size_t max
     gc::gcservice::GCServiceGlobalAllocator::CreateServiceAllocator();
     LOG(ERROR) << "Zygote: Done initializing with global allocator";
   }
-  live_bitmap_.reset(accounting::BaseHeapBitmap::CreateHeapBitmap(this,
-      (!Runtime::Current()->IsCompiler()) && GC_SERVICE_SHARABLE_HEAP_BITMAP));
-  mark_bitmap_.reset(accounting::BaseHeapBitmap::CreateHeapBitmap(this,
-      (!Runtime::Current()->IsCompiler()) && GC_SERVICE_SHARABLE_HEAP_BITMAP));
+  if(Runtime::Current()->IsCompiler()) {
+    live_bitmap_.reset(new accounting::HeapBitmap(this));
+    mark_bitmap_.reset(new accounting::HeapBitmap(this));
+  } else {
+    live_bitmap_.reset(accounting::BaseHeapBitmap::CreateHeapBitmap(this,
+        (!Runtime::Current()->IsCompiler()) && GC_SERVICE_SHARABLE_HEAP_BITMAP));
+    mark_bitmap_.reset(accounting::BaseHeapBitmap::CreateHeapBitmap(this,
+        (!Runtime::Current()->IsCompiler()) && GC_SERVICE_SHARABLE_HEAP_BITMAP));
+  }
+
 #else
   live_bitmap_.reset(new accounting::HeapBitmap(this));
   mark_bitmap_.reset(new accounting::HeapBitmap(this));
