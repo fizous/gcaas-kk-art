b5394e9adf41b8206d0511ac82c8e26fcd566642
abdelhaf <none@none>
2015-10-29 14:00:22 -0400

    AtomicStacks: walk the markbitmap from remote server

--- runtime/gc/collector/ipc_mark_sweep.cc
+++ runtime/gc/collector/ipc_mark_sweep.cc
@@ -845,12 +845,13 @@ void IPCMarkSweep::PostMarkingPhase(void){
   Thread* currThread = Thread::Current();
   ThreadList* thread_list = Runtime::Current()->GetThreadList();
   UpdateGCPhase(currThread, space::IPC_GC_PHASE_ROOT_POST_MARK);
-  LOG(ERROR) << "IPCMarkSweep::PostMarkingPhase: SSSSSSSSSSSSSSSSSSUspended the "
-      "threads: " << currThread->GetTid();
-  thread_list->SuspendAll();
-  LOG(ERROR) << "SSSSSSSSSSSSSSSSSSUspended the threads";
-  thread_list->ResumeAll();
-
+  if(0) {
+    LOG(ERROR) << "IPCMarkSweep::PostMarkingPhase: SSSSSSSSSSSSSSSSSSUspended the "
+        "threads: " << currThread->GetTid();
+    thread_list->SuspendAll();
+    LOG(ERROR) << "SSSSSSSSSSSSSSSSSSUspended the threads";
+    thread_list->ResumeAll();
+  }
   {
     ReaderMutexLock mu_mutator(currThread, *Locks::mutator_lock_);
     WriterMutexLock mu_heap_bitmap(currThread, *Locks::heap_bitmap_lock_);
