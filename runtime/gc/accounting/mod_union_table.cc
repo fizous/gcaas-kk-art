c9487e705163c2c25b1ae9b79e2eb0313d680e65
abdelhaf <none@none>
2015-11-09 18:48:34 -0500

    AtomicStackSharing: check whether cancelling atomic stack will solve the problem

--- runtime/gc/accounting/mod_union_table.cc
+++ runtime/gc/accounting/mod_union_table.cc
@@ -304,7 +304,7 @@ void ModUnionTableCardCache::MarkReferences(collector::MarkSweep* mark_sweep) {
     auto start = reinterpret_cast<uintptr_t>(card_table->AddrFromCard(card_addr));
     auto end = start + ConstantsCardTable::kCardSize;
     auto obj_start = reinterpret_cast<Object*>(start);
-    if(mark_sweep->IsInterprocess()) {
+    if(false && mark_sweep->IsInterprocess()) {
       LOG(ERROR) << "------ModUnionTableCardCache::MarkReferences---" <<
           reinterpret_cast<void*>(obj_start);
     }
