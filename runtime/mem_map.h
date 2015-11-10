4e23e35dab429a2ab8ca9947168180c82ef2a2d1
abdelhaf <none@none>
2015-11-10 17:30:36 -0500

    AtomicStackSharing: add server begin to the struct

--- runtime/mem_map.h
+++ runtime/mem_map.h
@@ -39,7 +39,6 @@ namespace art {
 typedef struct AShmemMap_S {
   char name_[64];
   byte* /*const*/ begin_;  // Start of data.
-  //byte* server_begin_;  // Start of data on the server side.
   size_t size_;  // Length of data.
   void* /*const*/ base_begin_;  // Page-aligned base address.
   /*const*/ size_t base_size_;  // Length of mapping.
@@ -48,7 +47,7 @@ typedef struct AShmemMap_S {
   int flags_;
   /*integer to hold the file descriptor of the memory mapped region */
   int fd_;
-  AShmemMap_S(const std::string& name, byte* begin, byte* server_begin,
+  AShmemMap_S(const std::string& name, byte* begin,/*, byte* server_begin,*/
       size_t size, void* base_begin, size_t base_size, int prot,
       int flags, int fd) :
         begin_(begin)/*, server_begin_(server_begin)*/, size_(size),
@@ -57,7 +56,7 @@ typedef struct AShmemMap_S {
     memcpy(name_, name.c_str(), name.size());
     name_[name.size()] = '\0';
   };
-  AShmemMap_S():begin_(NULL)/*, server_begin_(NULL)*/, size_(0), base_begin_(NULL),
+  AShmemMap_S():begin_(NULL), size_(0), base_begin_(NULL),
       base_size_(0), prot_(0), flags_(0), fd_(-1){
     name_[0] = '\0';
   }
@@ -145,7 +144,7 @@ class MemBaseMap {
 
   static void AShmemFillData(AShmemMap* addr, const std::string& name, byte* begin,
       size_t size, void* base_begin, size_t base_size, int prot, int flags, int fd) {
-    AShmemMap _data = {"\0", begin/*, NULL*/, size, base_begin, base_size, prot, flags, fd};
+    AShmemMap _data = {"\0", begin, size, base_begin, base_size, prot, flags, fd};
     memcpy(_data.name_, name.c_str(), name.size());
     _data.name_[name.size()] = '\0';
     memcpy(addr, &_data, SERVICE_ALLOC_ALIGN_BYTE(AShmemMap));
