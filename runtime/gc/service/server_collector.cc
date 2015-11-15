9720a20c6da217555ff954671ebef09804a96ef8
abdelhaf <none@none>
2015-11-15 14:54:28 -0500

    block the zygote process until the client is done..add atomic_loac to atomic_stack..make allocation stack swapped as in correct way...test ipc_server_sweep

--- runtime/gc/service/server_collector.cc
+++ runtime/gc/service/server_collector.cc
@@ -171,7 +171,7 @@ class ServerMarkReachableTask : public WorkStealingTask {
       LOG(ERROR) << " ++++ Phase TASK noticed change  ++++ " << self->GetTid()
           << " phase=" << curr_collector_addr_->gc_phase_;
 
-      if(false)
+      if(true)
         server_instant_->ipc_msweep_->MarkReachableObjects(curr_collector_addr_);
 
       curr_collector_addr_->gc_phase_ = space::IPC_GC_PHASE_MARK_RECURSIVE;
