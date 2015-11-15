77d7c019e7021d7604d40de2f2b9cc92cd9c93bd
abdelhaf <none@none>
2015-11-14 20:57:47 -0500

    create the server atomicStack..make all operations atomicfor the atomic_stack...disable resharing heapbitmaps

--- runtime/gc/heap.cc
+++ runtime/gc/heap.cc
@@ -1353,12 +1353,14 @@ void Heap::PostZygoteForkWithSpaceFork(bool shared_space) {
             reinterpret_cast<accounting::SharedHeapBitmap*>(mark_bitmap_.release()),
             &_struct_alloc_space->mark_heap_bitmap_data_));
           LOG(ERROR) << "Done Resharing mark_heap_bitmap_data_";
-        }
-        allocation_stack_.reset(accounting::ATOMIC_OBJ_STACK_T::ShareStack(allocation_stack_.release(),
-            &(_struct_alloc_space->alloc_stack_data_), true, max_allocation_stack_size_));
 
-        live_stack_.reset(accounting::ATOMIC_OBJ_STACK_T::ShareStack(live_stack_.release(),
-            &(_struct_alloc_space->live_stack_data_), true, max_allocation_stack_size_));
+          allocation_stack_.reset(accounting::ATOMIC_OBJ_STACK_T::ShareStack(allocation_stack_.release(),
+              &(_struct_alloc_space->alloc_stack_data_), true, max_allocation_stack_size_));
+
+          live_stack_.reset(accounting::ATOMIC_OBJ_STACK_T::ShareStack(live_stack_.release(),
+              &(_struct_alloc_space->live_stack_data_), true, max_allocation_stack_size_));
+        }
+
 
         static const size_t default_mark_stack_size = 128 * KB;
         mark_stack_.reset(accounting::ATOMIC_OBJ_STACK_T::ShareStack(mark_stack_.release(),
