fd1ae814d3f225162aa94d632729ea87a64b156c
abdelhaf <none@none>
2015-11-18 21:24:55 -0500

    putting processmarkstack to the ipc_mark_sweep...adding class references to the data.....ServerVisitFieldsReferences

--- runtime/gc/collector/ipc_server_sweep-inl.h
+++ runtime/gc/collector/ipc_server_sweep-inl.h
@@ -203,7 +203,7 @@ inline void IPCServerMarkerSweep::ServerScanObjectVisit(mirror::Object* obj,
 
   if (UNLIKELY(klass->IsArrayClass())) {
     if (klass->IsObjectArrayClass()) {
-      ServerVisitObjectArrayReferences(obj->AsObjectArray<mirror::Object>(), visitor);
+      ServerVisitObjectArrayReferences(mapped_obj->AsObjectArray<mirror::Object>(), visitor);
     }
   } else if (UNLIKELY(klass == java_lang_Class_client_)) {
     //ServerVisitClassReferences(klass, obj, visitor);
