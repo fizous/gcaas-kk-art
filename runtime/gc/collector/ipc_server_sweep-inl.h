5bb775f1afd1ca8512e121b732bb90f4c259e761
abdelhaf <none@none>
2015-11-23 09:01:02 -0500

    putting processmarkstack to the ipc_mark_sweep...adding class references to the data.....ServerVisitFieldsReferences

--- runtime/gc/collector/ipc_server_sweep-inl.h
+++ runtime/gc/collector/ipc_server_sweep-inl.h
@@ -514,7 +514,7 @@ void IPCServerMarkerSweep::ServerScanObjectVisit(const mirror::Object* obj,
   int mapped_class_type = GetMappedClassType(mapped_klass);
   if(UNLIKELY(mapped_class_type == 0)) {
     android_atomic_add(1, &(class_count_));
-    ServerVisitClassReferences(mapped_klass, mapped_object, visitor);
+    //ServerVisitClassReferences(mapped_klass, mapped_object, visitor);
   } else if (UNLIKELY(mapped_class_type == 1)) {
     android_atomic_add(1, &(array_count_));
     ServerVisitObjectArrayReferences(
@@ -522,7 +522,7 @@ void IPCServerMarkerSweep::ServerScanObjectVisit(const mirror::Object* obj,
 
   } else if (UNLIKELY(mapped_class_type == 2)){
     android_atomic_add(1, &(other_count_));
-    ServerVisitOtherReferences(mapped_klass, mapped_object, visitor);
+    //ServerVisitOtherReferences(mapped_klass, mapped_object, visitor);
     if(UNLIKELY(IsReferenceMappedClass(mapped_klass))) {
 
     }
