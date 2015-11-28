605efa322bf8ae0b2b99b54668917aff66550586
abdelhaf <none@none>
2015-11-27 22:24:40 -0500

    HeapBitmap: capturing the zygote metadata in a separate struct during the resharing process

--- runtime/gc/collector/ipc_mark_sweep.cc
+++ runtime/gc/collector/ipc_mark_sweep.cc
@@ -765,7 +765,7 @@ class ClientMarkObjectVisitor {
 //      Locks::mutator_lock_->AssertSharedHeld(Thread::Current());
 //      Locks::heap_bitmap_lock_->AssertExclusiveHeld(Thread::Current());
 //    }
-    if(false)
+    //if(false)
       mark_sweep_->MarkObject(ref);
   }
 
