4ba6a9377914fd8b781e2202d45aa11f705719fe
abdelhaf <none@none>
2015-11-27 21:16:16 -0500

    HeapBitmap: capturing the zygote metadata in a separate struct during the resharing process

--- runtime/gc/collector/ipc_server_sweep.cc
+++ runtime/gc/collector/ipc_server_sweep.cc
@@ -337,7 +337,7 @@ void IPCServerMarkerSweep::ProcessMarckStack() {
       break;
     }
     popped_oject = mark_stack_->PopBack();
-    ServerScanObject(popped_oject);
+    ServerScanObject(popped_oject, 0);
   }
 
   if(false) {
