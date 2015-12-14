f9b42cccee9ccbaea4fd1af5a7b12f23d6ef3cb2
abdelhaf <none@none>
2015-12-14 14:57:31 -0500

    initializing current live mark to NULL

--- runtime/gc/collector/ipc_server_sweep.cc
+++ runtime/gc/collector/ipc_server_sweep.cc
@@ -247,9 +247,9 @@ void IPCServerMarkerSweep::ServerSweepCallback(size_t num_ptrs, mirror::Object**
   if(freed_bytes != 0) {
 
   }
-  LOG(ERROR) << "ServerSweepCallback..objects: " << freed_objects <<
-      ", freed_bytes = " << freed_bytes << "; space::kRecentFreeCount = " <<
-      space::kRecentFreeCount;
+//  LOG(ERROR) << "ServerSweepCallback..objects: " << freed_objects <<
+//      ", freed_bytes = " << freed_bytes << "; space::kRecentFreeCount = " <<
+//      space::kRecentFreeCount;
 
   android_atomic_add(-freed_bytes,
       &(_mark_sweeper->client_rec_->sharable_space_->heap_meta_.sub_record_meta_.num_bytes_allocated_));
