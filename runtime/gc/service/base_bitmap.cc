3e3d2f0e7e0adce620682819d902c4e6e70f9af9
abdelhaf <none@none>
2015-12-15 17:45:05 -0500

    Use MADVISE..to reset and clear atomictacks

--- runtime/gc/service/base_bitmap.cc
+++ runtime/gc/service/base_bitmap.cc
@@ -333,13 +333,13 @@ void SharedSpaceBitmap::SetName(const std::string& name) {
 
 void SharedSpaceBitmap::Clear() {
   if (Begin() != NULL) {
-   // memset(Begin(), 0, Size());
+    memset(Begin(), 0, Size());
     // This returns the memory to the system.  Successive page faults will return zeroed memory.
-    int result = madvise(Begin(), Size(), MADV_DONTNEED);
+    //int result = madvise(Begin(), Size(), MADV_DONTNEED);
 //    int result = memset(Begin(), 0, Size());
-    if (result == -1) {
-      PLOG(FATAL) << "madvise failed";
-    }
+//    if (result == -1) {
+//      PLOG(FATAL) << "madvise failed";
+//    }
   }
 }
 
