d345bd49b971ac4b0e309994d6a053136ff72054
abdelhaf <none@none>
2016-01-13 13:51:42 -0500

    Merge VM CODE: ..... support bigger workload in default_profiler

--- runtime/gc_profiler/MProfiler.h
+++ runtime/gc_profiler/MProfiler.h
@@ -214,7 +214,7 @@ public:
 	 */
 	static const GCMMPProfilingEntry profilTypes[];
 
-	static const int kGCMMPMaxEventsCounts = 128;
+	static const int kGCMMPMaxEventsCounts = 1024;
 
 	//static AtomicInteger GCPTotalAllocBytes;
 
