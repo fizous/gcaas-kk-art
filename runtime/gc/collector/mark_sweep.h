086438dafdd039b6cdb9bc7352e8d52d234c92de
abdelhaf <none@none>
2016-01-12 21:22:37 -0500

    Merge VM CODE: ..... support bigger workload in default_profiler

--- runtime/gc/collector/mark_sweep.h
+++ runtime/gc/collector/mark_sweep.h
@@ -213,6 +213,40 @@ class MarkSweep : public GarbageCollector {
   void ScanObjectVisit(const mirror::Object* obj, const MarkVisitor& visitor)
       NO_THREAD_SAFETY_ANALYSIS;
 
+
+
+  size_t GetFreedBytes() const {
+    return freed_bytes_;
+  }
+  void IncFreedBytes(size_t val) {
+    freed_bytes_ += val;
+  }
+
+  size_t GetFreedLargeObjectBytes() const {
+    return freed_large_object_bytes_;
+  }
+
+  void IncFreedLargeObjectBytes(size_t val) {
+    freed_large_object_bytes_.fetch_add(val);
+  }
+
+  void IncFreedObjects(size_t val) {
+    freed_objects_.fetch_add(val);
+  }
+
+  size_t GetFreedObjects() const {
+    return freed_objects_;
+  }
+
+  size_t GetFreedLargeObjects() const {
+    return freed_large_objects_;
+  }
+
+  void IncFreedLargeObjects(size_t val) {
+    freed_large_objects_.fetch_add(val);
+  }
+
+#if (ART_GC_SERVICE)
   size_t GetFreedBytes() const {
     return stats_counters_->freed_bytes_;
   }
@@ -244,7 +278,8 @@ class MarkSweep : public GarbageCollector {
     android_atomic_add(static_cast<int32_t>(val), &(stats_counters_->freed_large_objects_));
   }
 
-#if (ART_GC_SERVICE)
+
+
   void IncTotalTimeNs(uint64_t param) {
     time_stats_->total_time_ns_ += param;
   }
