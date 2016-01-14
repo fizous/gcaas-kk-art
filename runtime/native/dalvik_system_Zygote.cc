60d4bd795e4b9ce5298cf84ef295996544d0116e
abdelhaf <none@none>
2016-01-14 13:01:52 -0500

    defaultProfiler...putting the initialization dalvik_vm_zygote

--- runtime/native/dalvik_system_Zygote.cc
+++ runtime/native/dalvik_system_Zygote.cc
@@ -685,6 +685,7 @@ static pid_t ForkAndSpecializeCommon(JNIEnv* env, uid_t uid, gid_t gid, jintArra
       if (java_se_name != NULL) {
           se_name.reset(new ScopedUtfChars(env, java_se_name));
           se_name_c_str = se_name->c_str();
+          LOG(ERROR) << "java_se_name: " << se_name_c_str;
 #if (ART_USE_GC_PROFILER || ART_USE_GC_PROFILER_REF_DIST)
           LOG(ERROR) << "java_se_name: " << se_name_c_str;
           VMProfiler::dvmGCMMProfPerfCountersVative(se_name->c_str());
