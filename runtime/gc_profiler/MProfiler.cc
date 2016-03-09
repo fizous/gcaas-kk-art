c9508f3785d231921a0712c7b0aec691830c9b6c
abdelhaf <none@none>
2016-03-09 18:27:37 -0500

    GCService: change place of starting VMProfiler

--- runtime/gc_profiler/MProfiler.cc
+++ runtime/gc_profiler/MProfiler.cc
@@ -221,7 +221,7 @@ inline void GCPauseThreadManager::MarkEndTimeEvent(GCMMP_BREAK_DOWN_ENUM evType)
 	if(busy_ > 0) {
 		if(curr_marker_->type != evType) {
 		  LOG(ERROR) << "XXXXXXXXXXXXXXXXX ERROR TYPE IS NOT MATCHING XXXXX curr_marker_->type :"
-		      << curr_marker_->type << ", paramType = evType";
+		      << curr_marker_->type << ", paramType = " << evType;
 			return;
 		}
 		curr_marker_->finalMarker = GCPauseThreadManager::GetRelevantRealTime();
