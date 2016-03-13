e61557d504c5de5fcc03e980c50a2e97545e06fa
abdelhaf <none@none>
2016-03-13 18:34:53 -0400

    GCService: add log messages to check heap size after collection

--- runtime/globals.h
+++ runtime/globals.h
@@ -97,9 +97,13 @@ const bool kIsTargetBuild = false;
 
 
 #if ART_GC_SERVICE
+ #if !defined(ART_GC_SERVICE_VERBOSE)
+  #define ART_GC_SERVICE_VERBOSE 1
+ #endif
+ #define IPC_MS_VLOG(severity)            if (UNLIKELY(ART_GC_SERVICE_VERBOSE))       ::art::LogMessage(__FILE__, __LINE__, severity, -1).stream()
 #else
-#define ART_GC_SERVICE_VERBOSE 0
-#define IPC_MS_VLOG(severity)            if (UNLIKELY(ART_GC_SERVICE_VERBOSE))       ::art::LogMessage(__FILE__, __LINE__, severity, -1).stream()
+ #define ART_GC_SERVICE_VERBOSE 0
+ #define IPC_MS_VLOG(severity)            if (UNLIKELY(ART_GC_SERVICE_VERBOSE))       ::art::LogMessage(__FILE__, __LINE__, severity, -1).stream()
 #endif
 
 }  // namespace art
