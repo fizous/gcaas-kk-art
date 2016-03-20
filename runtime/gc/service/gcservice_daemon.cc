040154fbda92d6f416355642364c8aa15f440dca
abdelhaf <none@none>
2016-03-20 17:36:10 -0400

    gcService: adding regex..parameterized dump

--- runtime/gc/service/gcservice_daemon.cc
+++ runtime/gc/service/gcservice_daemon.cc
@@ -174,7 +174,8 @@ int GCSrvcMemInfoOOM::parseOOMHeaderString(char* line, char* label,
   int length= 0;
   const char* res;
   char  output[256];
-  res =  regex_search("\\s+\\d+\\skB:\\s\\S+", line, &length);
+  res =  regex_search("(\\(*.\\[)[qQ]", "((a[Q]", &length);
+  //res =  regex_search("\\s+\\d+\\skB:\\s\\S+", line, &length);
   if(length > 0) {
     memcpy(output,res, length);
     output[length] = '\0';
