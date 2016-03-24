080ba29cd244cd47a6eafdb7e9e8db326af1ccd9
abdelhaf <none@none>
2016-03-24 19:30:37 -0400

    adding support of thread affinity

--- runtime/gc/service/global_allocator.h
+++ runtime/gc/service/global_allocator.h
@@ -449,7 +449,7 @@ typedef SafeMap<volatile int32_t, GCSrvceAgent*> ClientAgentsMap;
 
 class GCServiceDaemon {
   /* each five request we will read the global update */
-  static const int kcGCSrvcBulkRequestsThreshold = 5;
+  static const int kcGCSrvcBulkRequestsThreshold = 4;
 
 
   Thread*   thread_;
