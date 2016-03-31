2cb4f8a0279422256bad5c5f82055826dc2f57ec
abdelhaf <none@none>
2016-03-31 19:35:13 -0400

    GCService: ad handling of AllocGC..adding taskStealworkerqueue

--- runtime/gc/collector/ipc_mark_sweep.cc
+++ runtime/gc/collector/ipc_mark_sweep.cc
@@ -352,31 +352,43 @@ bool IPCHeap::CheckTrimming(collector::GcType gc_type, uint64_t gc_duration) {
 
   double _latency_rate_s = 0.0;
 
-  if(gc_duration > 0 && collection_latency_ > gc_duration
-      && GCServiceGlobalAllocator::allocator_instant_->isAddRemoteConcLatency()) {
-    double _ration_latency = ((collection_latency_ - gc_duration) * 1.0)  / gc_duration;
-    _latency_rate_s = _ration_latency * local_heap_->GetAllocationRate() * (gc_duration / 1000.0 / 1000);
+//  if(gc_duration > 0 && collection_latency_ > gc_duration
+//      && GCServiceGlobalAllocator::allocator_instant_->isAddRemoteConcLatency()) {
+//    double _ration_latency = ((collection_latency_ - gc_duration) * 1.0)  / gc_duration;
+//    _latency_rate_s = _ration_latency * local_heap_->GetAllocationRate() * (gc_duration / 1000.0 / 1000);
+//
+//    LOG(ERROR) << "_ration_latency = " << _ration_latency << ", allocRate="  << local_heap_->GetAllocationRate();
+//  }
 
-    LOG(ERROR) << "_ration_latency = " << _ration_latency << ", allocRate="  << local_heap_->GetAllocationRate();
+
+  if(GCServiceGlobalAllocator::allocator_instant_->isAddRemoteConcLatency()) {
+    _latency_rate_s = local_heap_->GetAllocationRate() * (collection_latency_ / 1000.0 / 1000.0);
+
+
+    LOG(ERROR) << "time_latency = " << collection_latency_
+        << ", alloc_latency=" << allocation_latency_ << ", HallocRate="
+        << local_heap_->GetAllocationRate() << ", latency_rate="
+        << _latency_rate_s;
+
+    _latency_rate_s = std::max(_latency_rate_s, 1.0 * 128 * KB);
+    LOG(ERROR) << "IPCHeap::CheckTrimming latency_rate " << _latency_rate_s;
+//    uint64_t _delta_time_ms =  NsToMs(collection_latency_);
+//    LOG(ERROR) << "IPCHeap::CheckTrimming alloc_latency: " << allocation_latency_
+//        << ", time_latency: " << collection_latency_ << ", _delta_time_ms = "
+//        << _delta_time_ms;
+//    if(GCServiceGlobalAllocator::allocator_instant_->isAddRemoteConcLatency()
+//        && _delta_time_ms > 0) {
+//      _latency_rate_s = (_delta_time_ms / 1000.0);
+//
+//      _latency_rate_s = ((allocation_latency_ * 1.0/* * 1000*/) / _delta_time_ms);
+//    }
   }
 
 
-  if(false) {
-    uint64_t _delta_time_ms =  NsToMs(collection_latency_);
-    LOG(ERROR) << "IPCHeap::CheckTrimming alloc_latency: " << allocation_latency_
-        << ", time_latency: " << collection_latency_ << ", _delta_time_ms = "
-        << _delta_time_ms;
-    if(GCServiceGlobalAllocator::allocator_instant_->isAddRemoteConcLatency()
-        && _delta_time_ms > 0) {
-      _latency_rate_s = ((allocation_latency_ * 1.0/* * 1000*/) / _delta_time_ms);
-    }
-  }
 
 
 
 
-
-  LOG(ERROR) << "IPCHeap::CheckTrimming latency_rate " << _latency_rate_s;
 
   local_heap_->GCSrvcGrowForUtilization(gc_type, gc_duration, _resize_factor,
                                         &_adjusted_max_free,
