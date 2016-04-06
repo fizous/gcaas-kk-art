e108561e2244e6f42365acb9292b0795c3debab1
abdelhaf <none@none>
2016-04-06 13:49:18 -0400

    GCPRofileFragmentation: test fragmentations

--- runtime/gc_profiler/MProfiler.h
+++ runtime/gc_profiler/MProfiler.h
@@ -746,8 +746,8 @@ class FragGCProfiler : public VMProfiler {
    void dumpProfData(bool isLastDump);
    void gcpPostMarkCollection(void);
 
-   void gcpAddFragSegment(size_t used_bytes, size_t seg) {
-     hitogramsData_->addObjectFast(seg);
+   void gcpAddFragSegment(size_t used_bytes, size_t freeLength) {
+     hitogramsData_->addObjectFast(used_bytes, freeLength);
    }
    void attachSingleThread(Thread*);
    void initMarkerManager(bool) {
