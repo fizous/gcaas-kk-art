3b73abec96d03b725370f4a242c4ad552f586bf1
abdelhaf <none@none>
2016-04-10 17:53:30 -0400

    adding gc-compactor..continue implementation of compactor

--- runtime/gc/collector/compactor.cc
+++ runtime/gc/collector/compactor.cc
@@ -213,7 +213,7 @@ void SpaceCompactor::startCompaction(void) {
             MapValueToServer<mirror::Object>(_origin_class, &ismapped);
         if(ismapped) {
 //          const byte* _raw_address = reinterpret_cast<const byte*>(_origin_class);
-          (const_cast<mirror::Object*>(ref.second))->setClass(down_cast<mirror::Class*>(const_cast<mirror::Object*>(new_addr)));
+          (const_cast<mirror::Object*>(ref.second))->SetClass(down_cast<mirror::Class*>(const_cast<mirror::Object*>(new_addr)));
           LOG(ERROR) << "correcting class of object.." <<
               reinterpret_cast<const void*>(ref.first) <<
               ", old_class=" << reinterpret_cast<const void*>(_origin_class) <<
